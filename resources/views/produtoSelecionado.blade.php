f80387122effd15b05c4eec8fe843bdad8733825
seedfelho <sergio.filho2@estudante.ifms.edu.br>
2023-11-06 16:46:50 -0400

    adicionado informações na pagina de item retirado

--- resources/views/produtoSelecionado.blade.php
+++ resources/views/produtoSelecionado.blade.php
@@ -9,22 +9,43 @@
 		<!-- <div class="col-md-8 col-md-offset-2"> -->
 		<div class="conteiner">
 			<div class="img-produtoSelecionado">
-            	<img src="{{ Voyager::image( $produto->foto ) }}" style="">
+				<img src="{{ Voyager::image( $produto->foto ) }}" style="">
 			</div>
 			<div class="informacoes">
 				<div class="descricaoProduto">
 					<h1 class="tituloProduto">{{ $produto->descricao }}</h1>
 				</div>
 				<div class="infPrimaria">
-					<p class="textProduto">Local encontrado: {!! $produto->local_encontrado !!}</p>
-					<p class="textProduto">Quem encontrou: {!! $produto->quem_encontrou !!}</p>
+					<div class="testF">
+						<p class="textProduto">Local encontrado:</p>
+						<p class="textProduto2">{!! $produto->local_encontrado !!}</p>
+					</div>
+					<div class="testF">
+						<p class="textProduto">Quem encontrou:</p>
+						<p class="textProduto2"> {!! $produto->quem_encontrou !!}</p>
+					</div>
 				</div>
 				<div class="infSecundaria">
-
+					@if ($produto->retirado == true)
+					<div>
+						<p class="infoRetirado">Informações de quem retirou:</p>
+						<div class="testF">
+							<p class="textProduto">Nome:</p>
+							<p class="textProduto2">{!! $produto->nome_retirada !!}</p>
+						</div>
+						<div class="testF">
+							<p class="textProduto">Email:</p>
+							<p class="textProduto2"> {!! $produto->email !!}</p>
+						</div>
+					</div>
+					@endif
 				</div>
-				<a class="alinha-box" href="/localRetirada">
-                    <button class="btn-ver">Onde retirar</button>
-                </a>
+				@if ($produto->retirado == false)
+					<a class="alinha-box" href="/localRetirada">
+						<button class="btn-ver">Onde retirar</button>
+					</a>
+				@endif
+
 			</div>
 		</div>
 	</div>
